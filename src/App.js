5280800d94c590ebcc44bec4a57b205774d3b0e3
sameer <sameerbhardiya@gmail.com>
2024-11-11 23:04:40 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Greeting from "./components/containers/greeting/Greeting";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Greeting/>
+    </div>
+  );
+}
+
+export default App;
